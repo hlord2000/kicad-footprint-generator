95be0c98b2e36764e4d5539c1d8a2b03ade98508
Simon F <spf85k@gmail.com>
2025-09-19 11:09:48 +0200

    Gullwing 3D + FP generator unification:
    See merge request https://gitlab.com/kicad/libraries/kicad-footprint-generator/-/merge_requests/1878
    
    Moved all yaml files of the footprint generator to their new locations.

--- scripts/Packages/Package_Gullwing__QFP_SOIC_SO/generate.sh
+++ scripts/Packages/Package_Gullwing__QFP_SOIC_SO/generate.sh
@@ -2,11 +2,13 @@
 
 set -ex
 
-
-for file in size_definitions/*.yaml; do
-    ./ipc_gullwing_generator.py "$file" -v
+for file in ../../../data/Gullwing/*.yaml; do
+    filename=$(basename "$file")
+    if [ "$filename" != "cq_parameters.yaml" ]; then
+        ./ipc_gullwing_generator.py "$file" -v
+    fi
 done
 
-for file in test_definitions/*.yaml; do
+for file in ../../../tests/generators/Gullwing/*.yaml; do
    ./ipc_gullwing_generator.py "$file" -v --output-dir "test_output"
 done
