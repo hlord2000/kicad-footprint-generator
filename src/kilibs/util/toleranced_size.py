1a95f8284aef2a7f92b3fbbb04916c0e64482cb8
Simon F <spf85k@gmail.com>
2025-10-03 20:15:49 +0000

    Correction of typing errror in TolerancedSize
    * Corrected error in TolerancedSize in an if-else cases.
    
    See merge request https://gitlab.com/kicad/libraries/kicad-footprint-generator/-/merge_requests/1888

--- src/kilibs/util/toleranced_size.py
+++ src/kilibs/util/toleranced_size.py
@@ -47,14 +47,16 @@ class TolerancedSize:
         if nominal is not None:
             self.nominal = nominal
         else:
-            if minimum is None and maximum is None:
-                raise KeyError("Either nominal or minimum or maximum must be given")
-            elif minimum is not None and maximum is not None:
-                self.nominal = (minimum + maximum) / 2
-            elif minimum is not None:
-                self.nominal = minimum
-            else:  # elif maximum is not None:
-                self.nominal = maximum
+            if minimum is None:
+                if maximum is None:
+                    raise KeyError("Either nominal or minimum or maximum must be given")
+                else:
+                    self.nominal = maximum
+            else:
+                if maximum is None:
+                    self.nominal = minimum
+                else:
+                    self.nominal = (minimum + maximum) / 2
 
         if minimum is not None and maximum is not None:
             self.minimum = minimum
